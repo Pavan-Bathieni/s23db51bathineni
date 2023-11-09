c8e4babafc7cf10986246ea0a7d5add20b59754f
Pavan-Bathieni <S566651@nwmissouri.edu>
2023-11-08 21:17:31 -0600

    final commit

--- controllers/watch.js
+++ controllers/watch.js
@@ -7,10 +7,27 @@ res.send('NOT IMPLEMENTED: watch list');
 exports.watch_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: watch detail: ' + req.params.id);
 };
-// Handle watch create on POST.
-exports.watch_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: watch create POST');
-};
+    // Handle watch create on POST.
+    exports.watch_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new watch();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"watch_type":"goat", "cost":12, "size":"large"}
+    document.WatchId = req.body.WatchId;
+    document.Watch_Price = req.body.Watch_Price;
+    document.Watch_Style = req.body.Watch_Style;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // Handle watch delete form on DELETE.
 exports.watch_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: watch delete DELETE ' + req.params.id);
