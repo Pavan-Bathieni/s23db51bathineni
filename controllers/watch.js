c2ae2c9dff42f1621407780d18cf162fb9e74dd7
Pavan-Bathieni <S566651@nwmissouri.edu>
2023-11-10 12:04:53 -0600

    Added New requestes

--- controllers/watch.js
+++ controllers/watch.js
@@ -1,14 +1,22 @@
 var watch = require('../models/watch');
 // List of all watch
-exports.watch_list = function(req, res) {
-res.send('NOT IMPLEMENTED: watch list');
+exports.watch_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: watch list');
 };
 // for a specific watch.
-exports.watch_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: watch detail: ' + req.params.id);
+exports.watch_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await watch.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
-    // Handle watch create on POST.
-    exports.watch_create_post = async function(req, res) {
+
+// Handle watch create on POST.
+exports.watch_create_post = async function (req, res) {
     console.log(req.body)
     let document = new watch();
     // We are looking for a body, since POST does not have query parameters.
@@ -18,44 +26,59 @@ res.send('NOT IMPLEMENTED: watch detail: ' + req.params.id);
     document.WatchId = req.body.WatchId;
     document.Watch_Price = req.body.Watch_Price;
     document.Watch_Style = req.body.Watch_Style;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    
+};
+
 // Handle watch delete form on DELETE.
-exports.watch_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: watch delete DELETE ' + req.params.id);
+exports.watch_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: watch delete DELETE ' + req.params.id);
 };
 // Handle watch update form on PUT.
-exports.watch_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: watch update PUT' + req.params.id);
+exports.watch_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await watch.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.WatchId)
+            toUpdate.WatchId = req.body.WatchId;
+        if (req.body.cost) toUpdate.Watch_Price = req.body.Watch_Price;
+        if (req.body.size) toUpdate.Watch_Style = req.body.Watch_Style;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
 };
 // List of all watch
-exports.watch_list = async function(req, res) {
-    try{
-    thewatch = await watch.find();
-    res.send(thewatch);
+exports.watch_list = async function (req, res) {
+    try {
+        thewatch = await watch.find();
+        res.send(thewatch);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    // Handle a show all view
-exports.watch_view_all_Page = async function(req, res) {
-try{
-thewatch = await watch.find();
-res.render('watch', { title: 'watch Search Results', watchList: thewatch });
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
 };
-    
+// Handle a show all view
+exports.watch_view_all_Page = async function (req, res) {
+    try {
+        thewatch = await watch.find();
+        res.render('watch', { title: 'watch Search Results', watchList: thewatch });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
